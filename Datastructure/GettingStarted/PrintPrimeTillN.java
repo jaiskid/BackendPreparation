c36421619c27941456d2cb9cc3a678f18b3fdcda
Neeraj Jaiswal <Neeraj001jaiswal@gmail.com>
2022-04-16 14:35:14 +0530

    Print Prime till N Time Complexity for N numbers it's Square root of N*(Sqrt(N))

---
+++ Datastructure/GettingStarted/PrintPrimeTillN.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class PrintPrimeTillN {
+	public static boolean PrintPrime(int n) {
+		int Number = n;
+		for (int i = 2; i * i <= n; i++) {
+			if ( Number % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		for (int i = 2; i <= n; i++) {
+			if (PrintPrime(i)) {
+				System.out.println(i);
+			}
+		}
+	}
+}
